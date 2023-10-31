586341a05ef4bd866f58eeb93b52ba7cd92020ce
Harshit <rajputharshit48@gmail.com>
2023-10-31 20:10:13 +0530

    second commit

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './header.module.css';
+import { Pets, Search } from '@mui/icons-material';
 
 function Header() {
   return (
@@ -7,6 +8,12 @@ function Header() {
       <div className={styles.logo}>
          <h1>logo</h1>
       </div>
+      <div className={styles.topBarCenter}>
+         <div className={styles.searchBar}>
+         <Pets color='warning'/>
+         <input placeholder='search for friend' className={styles.searchInput}/>
+         </div>
+       </div>
       
     </div>
   )
